14b56d1c75b13dbb0e9cf6777de7ca5b4c00c144
Lian Jung <lianjung@Lians-Air.fios-router.home>
2025-04-22 21:19:10 -0400

    authentication persists

--- client/src/store/useAuthStore.ts
+++ client/src/store/useAuthStore.ts
@@ -1,51 +1,58 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { axiosInstance } from "../lib/axios";
 import { UserForm } from "../types/UserForm";
 import { User } from "../types/User";
 import toast from "react-hot-toast";
+import { AuthStore } from "../types/AuthStore";
 
-export const useAuthStore = create((set) => ({
-  isAuthenticated: false,
-  setIsAuthenticated: (isAuthenticated: boolean) => set({ isAuthenticated }),
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      isAuthenticated: false,
+      user: null,
 
-  user: null,
-  setUser: (user: User) => set({ user }),
+      setIsAuthenticated: (isAuthenticated) => set({ isAuthenticated }),
+      setUser: (user) => set({ user }),
 
-  signup: async (user: UserForm) => {
-    try {
-      const response = await axiosInstance.post("/auth/register", user);
-      set({ user: response.data });
-      set({ isAuthenticated: true });
-      toast.success("Account created");
-    } catch (error) {
-      toast.error("Error creating account");
-      set({ user: null });
-      set({ isAuthenticated: false });
+      signup: async (user) => {
+        try {
+          const response = await axiosInstance.post("/auth/register", user);
+          set({ user: response.data, isAuthenticated: true });
+          toast.success("Account created");
+        } catch (error) {
+          toast.error("Error creating account");
+          set({ user: null, isAuthenticated: false });
+        }
+      },
+
+      login: async (user) => {
+        try {
+          const response = await axiosInstance.post("/auth/login", user);
+          set({ user: response.data, isAuthenticated: true });
+          toast.success("Login successful");
+        } catch (error) {
+          toast.error("Invalid credentials");
+          set({ user: null, isAuthenticated: false });
+        }
+      },
+
+      logout: async () => {
+        try {
+          await axiosInstance.post("/auth/logout");
+          set({ user: null, isAuthenticated: false });
+          toast.success("Logout successful");
+        } catch (error) {
+          set({ user: null, isAuthenticated: false });
+        }
+      },
+    }),
+    {
+      name: "auth-storage",
+      partialize: (state) => ({
+        isAuthenticated: state.isAuthenticated,
+        user: state.user,
+      }),
     }
-  },
-
-  login: async (user: UserForm) => {
-    try {
-      const response = await axiosInstance.post("/auth/login", user);
-      set({ user: response.data });
-      set({ isAuthenticated: true });
-      toast.success("Login successful");
-    } catch (error) {
-      toast.error("Invalid credentials");
-      set({ user: null });
-      set({ isAuthenticated: false });
-    }
-  },
-
-  logout: async () => {
-    try {
-      await axiosInstance.post("/auth/logout");
-      set({ user: null });
-      set({ isAuthenticated: false });
-      toast.success("Logout successful");
-    } catch (error) {
-      set({ user: null });
-      set({ isAuthenticated: false });
-    }
-  },
-}));
+  )
+);
